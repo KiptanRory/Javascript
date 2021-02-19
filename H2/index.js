138c369d13700ca1cc085dcb819cf78b99fe7f90
Rory <2126882@talnet.nl>
2021-02-19 11:53:34 +0100

    h2

---
+++ H2/index.js
@@ -0,0 +1,7 @@
+document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById("coolz").setAttribute("src", "../img/coolz.jpg")
+document.getElementById("zwollebol").setAttribute("src", "../img/bol.jpg")
+
+
+
