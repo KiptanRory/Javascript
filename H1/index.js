63967cad49b01b4542bb2ca89842d737423676ff
Rory <2126882@talnet.nl>
2021-02-19 11:01:09 +0100

    h1

--- H1/index.js
+++ H1/index.js
@@ -2,3 +2,10 @@ console.log("Helle World")
 for (var i = 0; i < 10; i++) {
     console.log("de waarde van i is "+ i)
 }
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw",
+}
+console.log(mijnauto["kleur"])
